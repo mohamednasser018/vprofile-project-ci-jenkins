93c413564c72e3142da52b34239a1c6c1b30e541
mohamed nasser <m0hamed.nass01r0@gmail.com>
2024-04-21 15:15:22 +0200

    update data user

--- userdata/nexus-setup.sh
+++ userdata/nexus-setup.sh
@@ -1,38 +1,60 @@
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y   
-mkdir -p /opt/nexus/   
-mkdir -p /tmp/nexus/                           
+
+# Install Java and wget
+apt update
+apt install openjdk-8-jdk wget -y
+
+# Set up JAVA_HOME environment variable
+JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+echo "JAVA_HOME=$JAVA_HOME" >> /etc/environment
+source /etc/environment
+
+# Create directories
+mkdir -p /opt/nexus/
+mkdir -p /tmp/nexus/
+
 cd /tmp/nexus/
+
+# Download Nexus
 NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
 wget $NEXUSURL -O nexus.tar.gz
-sleep 10
-EXTOUT=`tar xzvf nexus.tar.gz`
-NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
+
+# Extract Nexus
+tar xzvf nexus.tar.gz
+NEXUSDIR=$(tar tzf nexus.tar.gz | head -1 | cut -f1 -d'/')
 sleep 5
+
+# Clean up
 rm -rf /tmp/nexus/nexus.tar.gz
 cp -r /tmp/nexus/* /opt/nexus/
 sleep 5
-useradd nexus
-chown -R nexus.nexus /opt/nexus 
-cat <<EOT>> /etc/systemd/system/nexus.service
-[Unit]                                                                          
-Description=nexus service                                                       
-After=network.target                                                            
-                                                                  
-[Service]                                                                       
-Type=forking                                                                    
-LimitNOFILE=65536                                                               
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
-User=nexus                                                                      
-Restart=on-abort                                                                
-                                                                  
-[Install]                                                                       
-WantedBy=multi-user.target                                                      
 
+# Create nexus user
+useradd nexus
+chown -R nexus:nexus /opt/nexus
+
+# Create systemd service file
+cat <<EOT >> /etc/systemd/system/nexus.service
+[Unit]
+Description=Nexus service
+After=network.target
+
+[Service]
+Type=forking
+LimitNOFILE=65536
+ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start
+ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop
+User=nexus
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
 EOT
 
+# Configure Nexus to run as the nexus user
 echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
+
+# Reload systemd and start Nexus
 systemctl daemon-reload
 systemctl start nexus
 systemctl enable nexus
